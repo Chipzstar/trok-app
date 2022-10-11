0ecf7cd26cac195de984d5745d3341997d82bcb8
Chipzstar <chipzstar.dev@googlemail.com>
2022-10-11 14:34:29 +0100

    add restart signup button

--- apps/trok-dashboard/pages/index.tsx
+++ apps/trok-dashboard/pages/index.tsx
@@ -1,10 +1,17 @@
 import { Button } from '@mantine/core';
 import React from 'react';
+import { useRouter } from 'next/router';
 
-export function Index() {
+export function Index({ auth, setAuth }) {
+	const router = useRouter();
 	return (
 		<div className='h-screen flex flex-col justify-center p-5 overflow-hidden w-full'>
-			<Button color="red" size="lg" px="xl">Restart Signup</Button>
+			<div className='m-auto'>
+				<Button color='red' size='lg' px='xl' onClick={() => {
+					setAuth(false);
+					router.push('/signup');
+				}}>Restart Signup</Button>
+			</div>
 		</div>
 	);
 }
