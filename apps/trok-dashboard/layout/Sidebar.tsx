1470468923ac0b2a7a906d1f2e8f31635db55c4a
Chipzstar <chipzstar.dev@googlemail.com>
2022-10-12 15:00:08 +0100

    UI cleanup of dashboard sidebar

--- apps/trok-dashboard/layout/Sidebar.tsx
+++ apps/trok-dashboard/layout/Sidebar.tsx
@@ -18,9 +18,7 @@ const useStyles = createStyles((theme, _params, getRef) => {
 	return {
 		header: {
 			padding: theme.spacing.md,
-			paddingTop: 0,
-			marginLeft: -theme.spacing.md,
-			marginRight: -theme.spacing.md,
+			paddingTop: theme.spacing.xs,
 			color: theme.colorScheme === 'dark' ? theme.white : theme.black,
 			borderBottom: `1px solid ${
 				theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[3]
