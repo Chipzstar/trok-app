e3ffa141e4cca0c8eec226829fbf4b013fb7ace9
Chipzstar <chipzstar.dev@googlemail.com>
2022-10-18 20:21:39 +0100

    update Redis client configuration

--- apps/trok-backend/src/app/redis/index.ts
+++ apps/trok-backend/src/app/redis/index.ts
@@ -1,12 +1,15 @@
 import Redis from 'ioredis';
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
-const options = isDevelopment ? {
+/*const options = isDevelopment ? {
 	password: process.env.REDIS_PASSWORD,
 	username: 'default'
-} : {}
+}*/
 
-const redisClient = new Redis(Number(process.env.REDIS_PORT), String(process.env.REDIS_HOST), options);
+const redisClient = new Redis(Number(process.env.REDIS_PORT), String(process.env.REDIS_HOST), {
+	password: process.env.REDIS_PASSWORD,
+	username: process.env.REDIS_USERNAME
+});
 
 /*// if no connection, an error will be emitted
 // handle connection errors
