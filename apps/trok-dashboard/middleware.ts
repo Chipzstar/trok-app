c0acae532f997f80941427aca1cd948562474035
Chipzstar <chipzstar.dev@googlemail.com>
2022-10-20 16:44:35 +0100

    add login functionality to dashboard

--- apps/trok-dashboard/middleware.ts
+++ apps/trok-dashboard/middleware.ts
@@ -1,3 +1,3 @@
 export { default } from "next-auth/middleware"
 
-export const config = { matcher: ["/fleet/:path*"] }
+export const config = { matcher: ["/transactions/:path*", "/drivers/:path*", "/cards/:path*", "/payments/:path*", "/payment-method/:path*", "/statements/:path*", "/referral/:path*", "/settings/:path*"] }
