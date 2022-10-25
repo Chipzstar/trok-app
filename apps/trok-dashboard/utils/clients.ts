57307c15eb263e1f7d6b4763bff864ca143993f0
Chipzstar <chipzstar.dev@googlemail.com>
2022-10-25 13:42:46 +0100

    remove redundant trpc proxy

--- apps/trok-dashboard/utils/clients.ts
+++ apps/trok-dashboard/utils/clients.ts
@@ -1,16 +1,7 @@
 import axios from 'axios';
 import type { AppRouter } from '../../trok-backend/src/app/routes';
-import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
 import { createTRPCReact } from '@trpc/react';
 
 export const trpc = createTRPCReact<AppRouter>();
-
-export const trpcClient = createTRPCProxyClient<AppRouter>({
-	links: [
-		httpBatchLink({
-			url: 'http://localhost:3333/server/trpc',
-		}),
-	],
-});
 
 export const apiClient = axios.create()
