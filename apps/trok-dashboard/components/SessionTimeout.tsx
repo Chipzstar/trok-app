df37d9a8056ab4e5dd13446384ce8d57e12c1b37
Chipzstar <chipzstar.dev@googlemail.com>
2022-11-25 15:25:04 +0000

    only set session timeout interval when idle is true

--- apps/trok-dashboard/components/SessionTimeout.tsx
+++ apps/trok-dashboard/components/SessionTimeout.tsx
@@ -3,16 +3,17 @@ import { Button, Group, Modal, Stack, Text } from '@mantine/core';
 import { useCounter } from '@mantine/hooks';
 import { signOut } from 'next-auth/react';
 
+let interval;
 const SessionTimeout = ({ opened, onClose }) => {
 	const [count, handlers] = useCounter(60, { min: 0, max: 60 });
 	const countdownLogout = useCallback(() => handlers.decrement(), []);
 
 	useEffect(() => {
-		const interval = setInterval(countdownLogout, 1000);
+		if (opened) setInterval(countdownLogout, 1000);
 		return () => {
 			handlers.reset();
 			clearInterval(interval);
-		}
+		};
 	}, [opened]);
 
 	useEffect(() => {
