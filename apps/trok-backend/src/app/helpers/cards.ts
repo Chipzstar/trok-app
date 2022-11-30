9896ea6752c672b8abbbb210d358ce271abcda7b
Chipzstar <chipzstar.dev@googlemail.com>
2022-11-30 11:46:42 +0000

    add debug logs for checkCardDeliveredStatus scheduler

--- apps/trok-backend/src/app/helpers/cards.ts
+++ apps/trok-backend/src/app/helpers/cards.ts
@@ -9,6 +9,7 @@ export async function checkCardDeliveredStatus(override = false) {
 		// set buffer of max +1 hour and min -1 hour from current timestamp
 		const MAX = dayjs().add(1, 'h').unix();
 		const MIN = dayjs().subtract(1, 'h').unix();
+		console.table({MIN, MAX})
 		const card_ids = override
 			? await redisClient.zrange(CARD_REDIS_SORTED_SET_ID, Number.NEGATIVE_INFINITY, Number.POSITIVE_INFINITY, 'BYSCORE', (err, res) =>
 					console.log('RESULT:', res)
@@ -17,17 +18,18 @@ export async function checkCardDeliveredStatus(override = false) {
 					console.log('RESULT:', res)
 			  );
 		// mark all returned card shipping_statuses from "shipped" to "delivered"
-		card_ids.map(
-			async (id, index) =>
-				await prisma.card.update({
-					where: {
-						card_id: id
-					},
-					data: {
-						shipping_status: CARD_SHIPPING_STATUS.DELIVERED
-					}
-				})
-		);
+		card_ids.map(async (id, index) => {
+			const num_removed = await redisClient.zrem(CARD_REDIS_SORTED_SET_ID, id)
+			console.table({num_removed})
+			return await prisma.card.update({
+				where: {
+					card_id: id
+				},
+				data: {
+					shipping_status: CARD_SHIPPING_STATUS.DELIVERED
+				}
+			});
+		});
 		return true;
 	} catch (err) {
 		console.error(err);
