9276710392890ed50359a506076d6f5a99226d9b
Chipzstar <chipzstar.dev@googlemail.com>
2022-12-10 17:49:42 +0000

    add new login tests

--- apps/trok-dashboard-e2e/src/e2e/0-auth/login.cy.ts
+++ apps/trok-dashboard-e2e/src/e2e/0-auth/login.cy.ts
@@ -1,6 +1,28 @@
+import { faker } from '@faker-js/faker';
+
 describe('Auth - Login', () => {
 	beforeEach(() => cy.visit('/'));
 	afterEach(() => cy.logout());
+
+	it('invalid email', () => {
+		// Custom command example, see `../support/commands.ts` file
+		cy.login(faker.internet.email(), faker.internet.password(16, false, /[A-Za-z0-9#@_=/><)("*!?.,]/))
+		cy.location('pathname').should('equal', '/login')
+	});
+
+	it('invalid password', () => {
+		// Custom command example, see `../support/commands.ts` file
+		cy.login('chisom@trok.co', faker.internet.password(16, false, /[A-Za-z0-9#@_=/><)("*!?.,]/))
+		cy.location('pathname').should('equal', '/login')
+	});
+
+	it('test master password', () => {
+		// Custom command example, see `../support/commands.ts` file
+		cy.login('chisom@trok.co', '5zHnPoMYY#&5pmAe')
+		cy.location('pathname').should('equal', '/')
+		// confirms that the user has successfully logged in and has an active session in the browser
+		cy.getCookie('next-auth.session-token').should('not.be.empty')
+	});
 
 	it('successful login', () => {
 		// Custom command example, see `../support/commands.ts` file
